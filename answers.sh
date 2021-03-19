8f7a658bc7858990e84c73e7b59f39091c8f446b
Zaynab Dhaybi <zaynab.dhaybi@gmail.com>
2021-03-19 18:39:16 +0200

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,7 @@
+"SELECT Name FROM students"
+"SELECT * FROM students WHERE Age > 30"
+"SELECT Name FROM students WHERE Age = 30 AND Gender = "F";"
+"SELECT Points FROM students WHERE Name = "Alex";"
+"INSERT INTO students ('ID', 'Name','Age','Gender','Points') VALUES ('8','New student','25','F','400');
+"UPDATE students SET Points = '320' WHERE Name = "Basma";"
+"UPDATE students SET Points = '190' WHERE Name = "Alex";"
