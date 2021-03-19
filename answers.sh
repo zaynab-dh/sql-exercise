ed810b280a8c11af935e193dbd34d9b1096df6cd
Zaynab Dhaybi <zaynab.dhaybi@gmail.com>
2021-03-20 00:47:20 +0200

    Count & Filter

--- answers.sh
+++ answers.sh
@@ -17,19 +17,25 @@ CREATE TABLE graduates
 );
 
 
-INSERT INTO graduates (Name, Age, Gender, Points)
+1.INSERT INTO graduates (Name, Age, Gender, Points)
 SELECT Name, Age, Gender, Points FROM students Where Name = "Layal";
 
 
-UPDATE graduates SET Graduation = '08/09/2018' WHERE Name = 'Layal';
+2.UPDATE graduates SET Graduation = '08/09/2018' WHERE Name = 'Layal';
 
 
-DELETE FROM students WHERE Name = "Layal";
+3.DELETE FROM students WHERE Name = "Layal";
 
-SELECT employees.Name, employees.Company,companies.Date 
+1.SELECT employees.Name, employees.Company,companies.Date 
 FROM employees
 Join companies ON employees.Company=companies.Name;
 
-SELECT employees.Name FROM employees Join companies ON employees.Company=companies.Name WHERE companies.Date < "2000";
+2.SELECT employees.Name FROM employees Join companies ON employees.Company=companies.Name WHERE companies.Date < "2000";
 
-SELECT companies.Name FROM employees Join companies ON employees.Company=companies.Name WHERE employees.Role = "Graphic Designer";
+3.SELECT companies.Name FROM employees Join companies ON employees.Company=companies.Name WHERE employees.Role = "Graphic Designer";
+
+1.SELECT Name, max(Points) from students;
+2.SELECT avg(Points) from students;
+3.SELECT count(Name) from students WHERE points = "500";
+4.SELECT Name from students WHERE Name LIKE '%s%';
+5.SELECT Name from students ORDER BY points DESC;
